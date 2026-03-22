fab9f8a3dff2dd446bb2dcb7e5b75fe415614067
crgraduu <cringusradu24@stud.ase.ro>
2026-03-22 23:31:43 +0200

    Create Fisier.c

---
+++ Fisier.c
@@ -0,0 +1,81 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+struct StructuraMasina {
+	int id;
+	int nrUsi;
+	float pret;
+	char* model;
+	char* numeSofer;
+	unsigned char serie;
+};
+typedef struct StructuraMasina Masina;
+typedef struct Nod Nod;
+typedef struct ListaDubla ListaDubla;
+
+struct Nod {
+	Masina info;
+	Nod* next;
+	Nod* prev;
+};
+
+struct ListaDubla {
+	Nod* prim;
+	Nod* ultim;
+};
+
+Masina citireMasinaDinFisier(FILE* file) 
+{
+	char buffer[100];
+	char sep[3] = ",\n";
+	fgets(buffer, 100, file);
+	char* aux;
+	Masina m1;
+	aux = strtok(buffer, sep);
+	m1.id = atoi(aux);
+	m1.nrUsi = atoi(strtok(NULL, sep));
+	m1.pret = atof(strtok(NULL, sep));
+	aux = strtok(NULL, sep);
+	m1.model = malloc(strlen(aux) + 1);
+	strcpy_s(m1.model, strlen(aux) + 1, aux);
+
+	aux = strtok(NULL, sep);
+	m1.numeSofer = malloc(strlen(aux) + 1);
+	strcpy_s(m1.numeSofer, strlen(aux) + 1, aux);
+
+	m1.serie = *strtok(NULL, sep);
+	return m1;
+}
+
+void afisareMasina(Masina masina) 
+{
+	printf("Id: %d\n", masina.id);
+	printf("Nr. usi : %d\n", masina.nrUsi);
+	printf("Pret: %.2f\n", masina.pret);
+	printf("Model: %s\n", masina.model);
+	printf("Nume sofer: %s\n", masina.numeSofer);
+	printf("Serie: %c\n\n", masina.serie);
+}
+
+void afisareListaMasini(ListaDubla lista) {
+	Nod* p = lista.prim;
+	while (p) {
+		afisareMasina(p->info);
+		p = p->next;
+	}
+}
+
+int main() 
+{
+	FILE* f = fopen("masini.txt", "r");
+	Masina m1;
+	m1 = citireMasinaDinFisier(f);
+	afisareMasina(m1);
+	free(m1.model);
+	free(m1.numeSofer);
+	fclose(f);
+	return 0;
+}
